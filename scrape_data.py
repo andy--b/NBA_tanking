00df551bad4a28fae97cbd93b9c501c142093159
Andy <andrew.r.bowling@gmail.com>
2015-07-13 10:37:08 -0400

    Update getURLinfo fn to take a list of team names and years as args

--- scrape_data.py
+++ scrape_data.py
@@ -1,3 +1,5 @@
+#! python3
+
 import os
 import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
@@ -7,32 +9,39 @@ from selenium import webdriver
 
 from selenium.webdriver.firefox.webdriver import FirefoxProfile
 
-profile = FirefoxProfile("C:/Users/Andy/AppData/Roaming/Mozilla/Firefox/Profiles/cix7hlum.default")
-
-def getURLinfo(url):
+def getURLinfo(teams, years):
+# Takes a list of teams and list of year values and downloads game records for those teams/years
+	
+	# Set firefox to open with my profile
+	profile = FirefoxProfile\
+	("C:/Users/Andy/AppData/Roaming/Mozilla/Firefox/Profiles/cix7hlum.default")
 	browser = webdriver.Firefox(profile)
-
 	# Save the window opener (current window, do not mistaken with tab... not the same)
 	main_window = browser.current_window_handle
+	
+	# Loop for each team, each year
+	for team in teams:
+		for year in years:
+			# set url
+			url = "http://www.basketball-reference.com/teams/" \
+			+ str(team) + '/' + str(year) + '_games.html'
+			
+			# Open the link in a new tab by sending key strokes on the element
+			# Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack 
+			browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
 
-	# Open the link in a new tab by sending key strokes on the element
-	# Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack 
-	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+			# Put focus on current window which will, in fact, put focus on the current visible tab
+			browser.switch_to_window(main_window)
+			browser.get(url)
 
-	# Switch tab to the new tab, which we will assume is the next one on the right
-	# browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+			# Click on the "Export" button on page
+			browser.find_element_by_xpath\
+			("html/body/div[1]/div[4]/div[@id = 'all_schedule']/div[1]/div[1]/span[6]").click()
 
-	# Put focus on current window which will, in fact, put focus on the current visible tab
-	browser.switch_to_window(main_window)
-	browser.get(url)
-
-	# Click on the "Export" button on page
-	browser.find_element_by_xpath("html/body/div[1]/div[4]/div[@id = 'all_schedule']/div[1]/div[1]/span[6]").click()
-
-	# Close current tab
-	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
+			# Close current tab
+			browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
 
 	# driver.close()
 	
-getURLinfo("http://www.basketball-reference.com/teams/LAL/1995_games.html")
+getURLinfo(['CLE','CHI'],[1995,2006])
 

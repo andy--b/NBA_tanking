9f7c888114ea18b5c39fbb8dd0c1462f7dcf7e3f
Andy <andrew.r.bowling@gmail.com>
2015-07-13 13:41:49 -0400

    Moves downloaded csvs to organized folder structure. Ready for use!!
    Replace NBA TEAMS.xlsx and normal_teams.txt with teams_formatted.csv
    teams_formatted.csv was actually added in last commit but I forgot to mention

--- scrape_data.py
+++ scrape_data.py
@@ -1,8 +1,8 @@
 #! python3
 
 import os
-import csv
 import requests
+import shutil
 
 # Thank god for Selenium
 import selenium.webdriver as webdriver
@@ -10,6 +10,8 @@ import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import FirefoxProfile
+
+####### SET THE FOLLOWING VARIABLES PRIOR TO RUNNING THE PROGRAM
 
 # Specify your Firefox profile path here
 profile = FirefoxProfile\
@@ -21,11 +23,20 @@ profile = FirefoxProfile\
 # 2nd and 3rd cols: 4 digit start and end years, respectively
 INPUT_DATA="C:/Projects/NBA/teams_formatted.csv" 
 
+# Choose directory where the whole mess of team data will be stored
+# Individual subfolders will be created for each team
+OUTPUT_PATH="C:/Projects/NBA/game_data/"
+
+# Where do files download to on this computer?
+DOWNLOADS_FOLDER = 'C:/Users/Andy/Downloads/'
+
 
 def download_data(team_info):
 # Takes a 3 column list as argument. 1st column is team abbrev., 2nd and 3rd
 # are start and end years for that abbrev. 
-
+	
+	if not os.path.exists(OUTPUT_PATH): 
+		os.makedirs(OUTPUT_PATH)
 	
 	# Set firefox to open with my profile
 	profile = FirefoxProfile\
@@ -37,18 +48,23 @@ def download_data(team_info):
 	
 	# Loop for each team, each year
 	for team in team_info:
+		# Create subfolder for each team within the OUTPUT_PATH folder
+		team_path = str(OUTPUT_PATH) + '/' + team[0] + '/'
+		if not os.path.exists(team_path): 
+			os.makedirs(team_path)
+		
 		for year in range(int(team[1]),int(team[2]) + 1):
 			# set url
 			url = "http://www.basketball-reference.com/teams/" \
 			+ str(team[0]) + '/' + str(year) + '_games.html'
 			
-			print("Now reading ", url)
+			print("Now reading", url)
 			
 			#### These next 15 or so lines were done with SERIOUS help from StackOverflow
 			#### I've lost the page, but if I find it again I will attribute credit here.
 			
 			# Open the link in a new tab by sending key strokes on the element
-			Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack 
+			# Use: Keys.CONTROL + Keys.SHIFT + Keys.RETURN to open tab on top of the stack 
 			browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
 
 			# Put focus on current window which will, in fact, put focus on the current visible tab
@@ -57,12 +73,19 @@ def download_data(team_info):
 
 			# Click on the "Export" button on page
 			browser.find_element_by_xpath\
-			("html/body/div[1]/div[4]/div[@id = 'all_schedule']/div[1]/div[1]/span[6]").click()
-
+			("//div[@id = 'all_schedule']/div[1]/div[1]/span[6]").click()
 			# Close current tab
 			browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
-
-	driver.close()
+			
+			# Move file from downloads folder to destination folder
+			file_name = 'teams_' + team[0] + '_' + str(year) + '_games_teams_games.csv'
+			old_location = DOWNLOADS_FOLDER + file_name
+			new_location = team_path + file_name
+			
+			shutil.move(old_location, new_location)
+			
+	# Close browser at end of entire operation
+	browser.close()
 	
 	return None
 	

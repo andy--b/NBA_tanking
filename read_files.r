fa97d6d42e171b20c9da48f81753cb11486ae566
Andy <andrew.r.bowling@gmail.com>
2015-07-13 19:43:49 -0400

    Create read_files.r, which puts all .csv files into single list obj
    Also correct error in teams_formatted.csv that listed wrong abbrev
    For 2005 New Orleans team. Moved that file to its correct folder

---
+++ read_files.r
@@ -0,0 +1,49 @@
+#### Andy Bowling
+#### Take our downloaded/sorted NBA game data files and put them into
+#### a single list in R.
+
+
+# Read csv which has 3 columns
+# First column has team abbrev
+# 2nd has start year
+# 3rd has end year
+input_data = read.csv("C:/Projects/NBA/teams_formatted.csv", header = F);
+
+# We're storing all of our tables in a single list for easy referencing.
+game_data = list();
+
+# Assign variables for easy use
+team = input_data[,1];
+start_year = input_data[,2];
+end_year = input_data[,3];
+
+# Iterate through each team and year, assign list element
+for(te in 1:length(team)) {
+	for (y in start_year[te] : end_year[te]) {
+		# Set path for reading file, name for list item
+		path = paste("C:/Projects/NBA/game_data/", team[te], 
+		"/teams_", team[te], "_", as.character(y), "_games_teams_games.csv",
+		sep = '')
+		nam = paste(team[te], as.character(y), sep = '_');
+		# Clean up data a little bit. Remove useless/blank columns, get headers right
+		raw_team_data = read.csv(path, header = T);
+		raw_team_data = droplevels(raw_team_data);
+		
+		# Every 21st line in the file (21,42,etc) has headers. remove these
+		number_headers = as.integer(length(raw_team_data)/21);
+		header_lines = c(1:number_headers) * 21
+		raw_team_data = raw_team_data[,-header_lines]
+		
+		team_data = raw_team_data[,c("G","Date","Opponent","Tm","Opp","W","L","Streak")];
+		print(paste("Now Reading:", path));
+		game_data[[nam]] = team_data;
+		y = y + 1;
+		}
+	te = te + 1;
+	}
+
+# Clean up workspace
+rm(input_data,raw_team_data,team_data,header_lines,nam,number_headers,path,te,y)
+	
+# Hooray!	
+print("Reading Complete.")

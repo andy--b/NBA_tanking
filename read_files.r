b9e694804ae1c871e3666271e7e3939e3050b06b
Andy <andrew.r.bowling@gmail.com>
2015-07-14 15:31:06 -0400

    Change read_files.r to be a function that returns list

--- read_files.r
+++ read_files.r
@@ -3,53 +3,58 @@
 #### a single list in R called game_data.
 
 
+## SET THIS PATH BEFORE RUNNING
 # Read csv which has 3 columns
 # First column has team abbrev
 # 2nd has start year
 # 3rd has end year
 input_data = read.csv("C:/Projects/NBA/teams_formatted.csv", header = F);
 
-# We're storing all of our tables in a single list for easy referencing.
-game_data = list();
+read_files = function(input_data){
+	# We're storing all of our tables in a single list for easy referencing.
+	game_data = list();
 
-# Assign variables for easy use
-team = input_data[,1];
-start_year = input_data[,2];
-end_year = input_data[,3];
+	# Assign variables for easy use
+	team = input_data[,1];
+	start_year = input_data[,2];
+	end_year = input_data[,3];
 
-# Iterate through each team and year, assign list element
-for(te in 1:length(team)) {
-	for (y in start_year[te] : end_year[te]) {
-		# Set path for reading file, name for list item
-		path = paste("C:/Projects/NBA/game_data/", team[te], 
-		"/teams_", team[te], "_", as.character(y), "_games_teams_games.csv",
-		sep = '')
-		nam = paste(team[te], as.character(y), sep = '_');
-		
-		# Clean up data a little bit. Remove useless/blank columns, get headers right
-		# Read as.is, so we don't get factors/levels in our data
-		raw_team_data = read.csv(path, header = T, as.is = T);
-		
-		# Every 21st line in the file (21,42,etc) has headers. remove these
-		number_headers = as.integer(nrow(raw_team_data)/21);
-		header_lines = c(1:number_headers) * 21
-		raw_team_data = raw_team_data[-header_lines,]
-		
-		# Since we read as.is, we need to convert numerical columns
-		for (i in c("G","Tm","Opp","W","L"))
-			{raw_team_data[,i] = as.integer(raw_team_data[,i]);}
-		
-		# team_data 
-		team_data = raw_team_data[,c("G","Date","Opponent","Tm","Opp","W","L","Streak")];
-		print(paste("Now Reading:", path));
-		game_data[[nam]] = team_data;
-		y = y + 1;
+	# Iterate through each team and year, assign list element
+	for(te in 1:length(team)) {
+		for (y in start_year[te] : end_year[te]) {
+			# Set path for reading file, name for list item
+			path = paste("C:/Projects/NBA/game_data/", team[te], 
+			"/teams_", team[te], "_", as.character(y), "_games_teams_games.csv",
+			sep = '')
+			nam = paste(team[te], as.character(y), sep = '_');
+			
+			# Clean up data a little bit. Remove useless/blank columns, get headers right
+			# Read as.is, so we don't get factors/levels in our data
+			raw_team_data = read.csv(path, header = T, as.is = T);
+			
+			# Every 21st line in the file (21,42,etc) has headers. remove these
+			number_headers = as.integer(nrow(raw_team_data)/21);
+			header_lines = c(1:number_headers) * 21
+			raw_team_data = raw_team_data[-header_lines,]
+			
+			# Since we read as.is, we need to convert numerical columns
+			for (i in c("G","Tm","Opp","W","L"))
+				{raw_team_data[,i] = as.integer(raw_team_data[,i]);}
+			
+			# team_data 
+			team_data = raw_team_data[,c("G","Date","Opponent","Tm","Opp","W","L","Streak")];
+			print(paste("Now Reading:", path));
+			game_data[[nam]] = team_data;
+			y = y + 1;
+			}
+		te = te + 1;
 		}
-	te = te + 1;
+		
+	# Hooray!	
+	print("Reading Complete.");
+	
+	return(game_data);
 	}
 
-# Clean up workspace
-rm(input_data,raw_team_data,team_data,header_lines,nam,number_headers,path,te,y)
-	
-# Hooray!	
-print("Reading Complete.")
+# Run function with team/start year/end year input data
+game_data = read_files(input_data);
